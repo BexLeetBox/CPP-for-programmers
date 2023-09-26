9478210b8928e547459a6cbb32a57acceb7ab0e2
Bexter <prisombex@gmail.com>
2023-09-27 00:30:04 +0200

    Exercise two with a gui is working

---
+++ Container_Auto_Lambda/foo.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jerry on 9/27/2023.
+//
+
+#include "foo.h"
---
+++ Container_Auto_Lambda/oppgave2.cpp
@@ -0,0 +1,54 @@
+#include <gtkmm.h>
+
+class Window : public Gtk::Window {
+public:
+    Gtk::Box box;
+    Gtk::Label label_first_name;
+    Gtk::Label label_last_name;
+    Gtk::Entry entry_first_name;
+    Gtk::Entry entry_last_name;
+    Gtk::Button button_combine;
+    Gtk::Label label_full_name;
+
+    Window() : box(Gtk::Orientation::ORIENTATION_VERTICAL) {
+        set_title("Name Combiner");
+
+        label_first_name.set_text("First Name:");
+        label_last_name.set_text("Last Name:");
+
+        button_combine.set_label("Combine Names");
+        button_combine.set_sensitive(false); // Set button insensitive by default
+
+        // Packing the labels and entries into the box
+        box.pack_start(label_first_name, Gtk::PACK_SHRINK);    // Add the label for the first name
+        box.pack_start(entry_first_name, Gtk::PACK_SHRINK);    // Add the widget for the first name
+        box.pack_start(label_last_name, Gtk::PACK_SHRINK);     // Add the label for the last name
+        box.pack_start(entry_last_name, Gtk::PACK_SHRINK);     // Add the widget for the last name
+        box.pack_start(button_combine, Gtk::PACK_SHRINK);      // Add the combine button
+        box.pack_start(label_full_name, Gtk::PACK_SHRINK);     // Add the label to show the full name
+
+        add(box);                            // Add vbox to window
+        show_all();                          // Show all widgets
+
+        entry_first_name.signal_changed().connect(sigc::mem_fun(*this, &Window::on_entry_changed));
+        entry_last_name.signal_changed().connect(sigc::mem_fun(*this, &Window::on_entry_changed));
+
+        button_combine.signal_clicked().connect([this]() {
+            label_full_name.set_text("Full Name: " + entry_first_name.get_text() + " " + entry_last_name.get_text());
+        });
+    }
+
+    void on_entry_changed() {
+        if(!entry_first_name.get_text().empty() && !entry_last_name.get_text().empty()) {
+            button_combine.set_sensitive(true);
+        } else {
+            button_combine.set_sensitive(false);
+        }
+    }
+};
+
+int main() {
+    auto app = Gtk::Application::create();
+    Window window;
+    return app->run(window);
+}

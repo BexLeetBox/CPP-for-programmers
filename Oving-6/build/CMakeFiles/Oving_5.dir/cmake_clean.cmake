e436485c2e69f845c8d0c4f93214705f7047a99f
DedCon <prisombex@gmail.com>
2023-10-06 02:31:26 +0200

    completed chess with lambda

---
+++ Oving-6/build/CMakeFiles/Oving_5.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Oving_5.dir/Main.cpp.o"
+  "CMakeFiles/Oving_5.dir/Main.cpp.o.d"
+  "Oving_5"
+  "Oving_5.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Oving_5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

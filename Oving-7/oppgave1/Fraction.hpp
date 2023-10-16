35df3c6ccd4a64c24bb338839aaa2347409ccbbb
Bexter <prisombex@gmail.com>
2023-10-17 00:05:51 +0200

    first commit

---
+++ Oving-7/oppgave1/Fraction.hpp
@@ -0,0 +1,49 @@
+//
+// Created by jerry on 10/16/2023.
+//
+
+#ifndef OVING_7_FRACTION_HPP
+#define OVING_7_FRACTION_HPP
+
+
+#pragma once
+
+class Fraction {
+public:
+    int numerator;
+    int denominator;
+
+    Fraction();
+    Fraction(int numerator, int denominator);
+
+
+    void set(int numerator_, int denominator_ = 1);
+    Fraction operator+(const Fraction &other) const;
+    Fraction operator-(const Fraction &other) const;
+    Fraction operator*(const Fraction &other) const;
+    Fraction operator/(const Fraction &other) const;
+    Fraction operator-() const;
+    Fraction operator-(int value) const;
+    // Outside the class declaration
+    Fraction operator-(int value, const Fraction& fraction);
+
+    Fraction &operator++(); // Preinkrement
+    Fraction &operator--(); // Predekrement
+    Fraction &operator+=(const Fraction &other);
+    Fraction &operator-=(const Fraction &other);
+    Fraction &operator*=(const Fraction &other);
+    Fraction &operator/=(const Fraction &other);
+    Fraction &operator=(const Fraction &other);
+    bool operator==(const Fraction &other) const;
+    bool operator!=(const Fraction &other) const;
+    bool operator<=(const Fraction &other) const;
+    bool operator>=(const Fraction &other) const;
+    bool operator<(const Fraction &other) const;
+    bool operator>(const Fraction &other) const;
+
+private:
+    void reduce();
+    int compare(const Fraction &other) const;
+};
+
+#endif //OVING_7_FRACTION_HPP
---
+++ Oving-7/oppgave1/main.cpp
@@ -0,0 +1,50 @@
+#include "Fraction.hpp"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void print(const string &text, const Fraction &broek) {
+    cout << text << broek.numerator << " / " << broek.denominator << endl;
+}
+
+int main() {
+    Fraction a(10, 20);
+    Fraction b(3, 4);
+    Fraction c;
+    c.set(5);
+    Fraction d = a / b;
+
+    print("a = ", a);
+    print("b = ", b);
+    print("c = ", c);
+    print("d = ", d);
+
+    b += a;
+    ++c;
+    d *= d;
+
+    print("b = ", b);
+    print("c = ", c);
+    print("d = ", d);
+
+    Fraction e = a - 5;  // This will use the member function
+    Fraction f = 5 - a;  // This will use the non-member function
+
+    print("10/20 - 5 = ", e);
+    print("5 - 10/20 = ", f);
+
+
+    c = a + b - d * a;
+    c = -c;
+
+    print("c = ", c);
+
+    if (a + b != c + d)
+        cout << "a + b != c + d" << endl;
+    else
+        cout << " a + b == c + d" << endl;
+    while (b > a)
+        b -= a;
+    print("b = ", b);
+}
